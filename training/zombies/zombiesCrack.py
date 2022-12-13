755fbeeb44e74c7e8adc76a983bd74c53dda8e1a
harel12380 <harelbarhai967@gmail.com>
2022-12-13 09:15:36 +0200

    fix the python code that crack the zombies from 2022

--- training/zombies/zombiesCrack.py
+++ training/zombies/zombiesCrack.py
@@ -1,15 +1,43 @@
+def generateList():
+    res = []
+    temp = int("1234", 16)
+    for i in range(int("35", 16)):
+        temp = int(str(
+            hex(
+                (temp + int("4743", 16)) *
+                int("11bf", 16)
+            )
+        )[-4:], 16)
+        res += [str(temp)]
+    res.reverse()
+    return (res, temp)
 
-start = int("7df9", 16)
-toSub = int("4743", 16)
-res = int("4aca", 16)
-ans = []
+def parse(num):
+  # parse number and return his value as integer
+  # but only its 4 last digits in hex
+  return int(str(hex(num)[2:])[-4:], 16)
 
+arr, start = generateList()
+isFail = False
+finish = False
+
+# run on all of the numbers
 for i in range(1, int("ffff", 16)):
-  for j in range(1, int("ffff", 16)):
-    if (str(hex((start + j) * i))[-4:] == hex(res)[-4:]):
-      ans += [i, j]
-      print(ans)
-print("result is: " + ans)  
-
-#int("", 16);
-#hex(42)
+    print(i)
+    for j in range(1, int("ffff", 16)):
+      # reset variables
+      tempStart = start
+      isFail = False
+      # run and compare to the original array (<arr>)
+      for k in range(1, int("35", 16)):
+        currentCalc = parse(parse(tempStart * i) + j)
+        tempStart = currentCalc
+        if str(currentCalc) != arr[k]:
+            isFail = True
+            break
+      if isFail == False:
+          print("Mul by: ", i, '\t', "Add: ", j)
+          finish = True
+          break
+    if finish:
+        break
