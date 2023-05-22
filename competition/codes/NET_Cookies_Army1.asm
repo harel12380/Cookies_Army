64e37fe16b8dd333e693e3800afe9b2759535ef9
harel <harelbarhai967@gmail.com>
2023-05-22 17:36:00 +0300

    Upgrade codes for competition to v0.0.5

--- competition/codes/NET_Cookies_Army1.asm
+++ competition/codes/NET_Cookies_Army1.asm
@@ -1,55 +1,20 @@
-; / survivor 1 / ;
-; / changeable / ;
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; |defines|
-%define firstAttackOffset 0x200
-%define firstATtackLowerByte 0x5A
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
 push es
 pop ds
 
-push cs
-push cs; push es
-push cs
-pop es
-
 lodsw
-mov al, firstATtackLowerByte ; the lower byte for the attack start
-xchg bp, ax
+mov bx, ax
 
-mov ax, 0x
-mov dx, 0x
-int 0x87
-mov di, 0x
-int 0x86
-mov di, 0x
-int 0x86
-
+push cs
+push cs
 pop es
 pop ss
 
-lea bp, [bp + firstAttackOffset] ; where the first attack
-mov sp, bp
-mov di, bp
+lea di, [bx + @end]
+mov sp, di
 
-mov bp, 0xD4FF
-mov dx, 0x5355
+mov ax, 0x52AB
+mov dx, 0xABAB
 
-mov bx, 0xABAB
-mov cx, 0x5251
-
-mov ax, 0xABAB ; what I will write foward
-;mov di, 0x
-;mov si, 0x
-
-@attack:
-push bp
-push bx
-push cx
 push dx
-call sp
-
+stosw
 @end:
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
